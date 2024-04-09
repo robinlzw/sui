8066b33346bc8204db032a43ea0a3562a1baf498
edy <robin@movebit.xyz>
2024-04-09 17:20:18 +0800

    only need thread lock

--- external-crates/move/crates/move-package/src/package_lock.rs
+++ external-crates/move/crates/move-package/src/package_lock.rs
@@ -2,17 +2,9 @@
 // Copyright (c) The Move Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-use named_lock::{NamedLock, NamedLockGuard};
 use once_cell::sync::Lazy;
 use std::sync::{Mutex, MutexGuard};
-use whoami::username;
-
-const PACKAGE_LOCK_NAME: &str = "move_pkg_lock";
 static PACKAGE_THREAD_MUTEX: Lazy<Mutex<()>> = Lazy::new(|| Mutex::new(()));
-static PACKAGE_PROCESS_MUTEX: Lazy<NamedLock> = Lazy::new(|| {
-    let user_lock_file = format!("{}_{}", PACKAGE_LOCK_NAME, username());
-    NamedLock::create(user_lock_file.as_str()).unwrap()
-});
 
 /// The package lock is a lock held across threads and processes. This lock is held to ensure that
 /// the Move package manager has a consistent (read: serial) view of the file system. Without this
@@ -25,14 +17,12 @@ static PACKAGE_PROCESS_MUTEX: Lazy<NamedLock> = Lazy::new(|| {
 /// this is why we grab both a thread lock and process lock.
 pub(crate) struct PackageLock {
     _thread_lock: MutexGuard<'static, ()>,
-    _process_lock: NamedLockGuard<'static>,
 }
 
 impl PackageLock {
     pub(crate) fn lock() -> PackageLock {
         Self {
             _thread_lock: PACKAGE_THREAD_MUTEX.lock().unwrap(),
-            _process_lock: PACKAGE_PROCESS_MUTEX.lock().unwrap(),
         }
     }
 }
